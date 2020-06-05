d330695f0b743f6c72e6a91229d26bb0bcef567d
tima <tima_3015@mail.ru>
2020-06-05 15:23:57 +0300

    вторая домашка

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 10000;
+let income = 5000;
+let addExpenses = 'Проезд, кино, кафе ';
+let deposit = true;
+let mession = 1000000;
+let period = 12;
 
-alert('Первая домашка');
-
-console.log('Hello world');
-
+console.log( typeof (money) );
+console.log( typeof (income) );
+console.log( typeof (deposit) );
+console.log( addExpenses.length );
+console.log( 'Период равен ' + period + ' месяцев' );
+console.log( 'Цель заработать ' + mession + ' рублей' );
+console.log( addExpenses.toLocaleLowerCase() );
+console.log( addExpenses.split(', ' ) );
+let budgetDay = Math.round(money / 30); // не много отсебятины , так число получается не красивое 
+console.log('budgetDay: ', budgetDay);
