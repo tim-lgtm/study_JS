959492dd62470e78d8c15c07d3f739b7e5e17de4
tima <tima_3015@mail.ru>
2020-06-10 15:36:57 +0300

    жду корректировок

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let money = +prompt('Ваш месячный доход?', '50000');
+let money;
 let income = 'фриланс';
 let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
 let deposit = confirm('Есть ли у вас депозит в банке ?');
@@ -8,6 +8,16 @@ let deposit = confirm('Есть ли у вас депозит в банке ?');
 let showTypeOF = function(data){
     console.log(data, typeof(data));
 }
+
+function start(){
+    do{
+        money = +prompt('Ваш месячный доход?', '50000');
+    } while(isNaN(parseFloat(money))){
+
+        return money; 
+    }
+};
+start();
 
 showTypeOF(money);
 showTypeOF(income);
@@ -21,31 +31,44 @@ console.log( addExpenses.length );
 
 
 let expenses1 = prompt('Введите обязательную статью расходов', 'Курсы ');
-let amounth1 = +prompt('Во сколько это обойдётся?', '5000')
+let amounth1; 
 let expenses2 = prompt('Введите ещё одну обязательную статью расходов', 'Книги');
-let amounth2 = +prompt('Во сколько это обойдётся?', '2000')
-function getExpensesMonth(amounth1, amounth2){
-    return amounth1 + amounth2;
-}
-console.log( 'Обязательные расходы на месяц составят ' + getExpensesMonth(amounth1, amounth2));
+let amounth2;
 
-let mession = 100000;
+
+function getExpensesMonth(amounth1, amounth2){
+    do{
+        amounth1 = +prompt('Во сколько обойдётся?', '5000');
+        amounth2 = +prompt('Во сколько это обойдётся?', '2000');
+    }while(isNaN(parseFloat(amounth1, amounth2)));
+        console.log(amounth1);
+        return amounth1 + amounth2;
+}
+let expensesAmount = getExpensesMonth()
+console.log( 'Обязательные расходы на месяц составят ' + expensesAmount);
+
+let mession = -100000;
 console.log( 'Цель заработать ' + mession + ' рублей' );
 
 //вот тут сразу вопрос, можно-ли сделать это через callback функицию? 
-function getAccumulatedMonth(money, amounth1, amounth2  ){
-    return money - amounth1 - amounth2;
+function getAccumulatedMonth(money, expensesAmount){
+    return money - expensesAmount;
 }
-let accumulatedMonth = getAccumulatedMonth(money, amounth1, amounth2)
+let accumulatedMonth = getAccumulatedMonth(start(), expensesAmount );
 console.log('Накопления за месяц ' + accumulatedMonth);
 
 let period = 12;
 console.log( 'Период равен ' + period + ' месяцев' );
 
 function getTargetMonth(mession, accumulatedMonth){
-    return Math.ceil(mession / accumulatedMonth); 
-}
-console.log( 'Чтобы накопить,  вам потребуется: ' + getTargetMonth(mession, accumulatedMonth ) + ' месяцев');
+    let result = Math.ceil(mession / accumulatedMonth);
+    if(result > 0){
+        console.log('Цель будет достигнута за: ' + result + ' месяцев');
+    } else{
+        console.log('Произошла ошибка');
+    } 
+};
+getTargetMonth(mession, accumulatedMonth);
 
 let budgetDay = Math.floor(accumulatedMonth / 30);
 console.log('Бюджет на день: ' + budgetDay );
